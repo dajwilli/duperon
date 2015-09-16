fb794a778f25f09c01ad7b851e2d7f1cae3ea05b
David <David@JohnS-MBP.local>
2015-09-16 12:22:23 -0400

    added tabs, finsihed form and table, started js function for setting variables

--- duperon/js/init.js
+++ duperon/js/init.js
@@ -1,13 +1,28 @@
 jQuery(document).ready(function(){
-  $( "#hp-motor,#energy-rate" ).keyup(function() {
+  $( "input" ).keyup(function() {
+    this.value = this.value.replace(/[^0-9\.]/g,'');
     calculateTotals(this);
   });
     
 });
 
+function setVariables() {
+  var proposed_price = parseInt($("#proposed-sale").val());
+  return proposed_price;
+}
+
 function calculateTotals(clicked_object)
 {
-  var hp_motor = $( clicked_object ).val();
+  var test = setVariables();
+  var hp_motor = parseInt($( clicked_object ).val());
+  var energy_rate = parseInt($( clicked_object ).val());
   $( "#val1" ).text( hp_motor );
-    console.log("hit");
+  $('#ic-cost-1').text(test);
+  console.log($(clicked_object).data('name'));
+  console.log(test);
 }
+
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
